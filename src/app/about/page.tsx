eae2f68dd77e40f0166f746424a483cd8013c9f0
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-04-25 17:19:37 +0200

    Updated pages
    - Added shared layouts between pages
    - Added a few animations to header
    - Added a favicon
    - Added a logo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,25 @@
-// src/app/page.tsx
-export default function HomePage() {
-    return (
-      <main>
-        {/* discription about just adding some armored vehicles as demonstration */}
-        <h1>About us</h1>
-        <h2>Why did I pick Enlisted vehciles as a subject?</h2>
-        <p>
-          I am a huge fan of the game Enlisted, and I wanted to create a guide
-          for the armored vehicles in the game. I wanted to share my knowledge
-          and help others understand the vehicles better.
-        </p>
-        <h2>What is this website about?</h2>
-        <p>
-          This website is a guide for the armored vehicles in the game Enlisted.
-          It includes information about the vehicles, their stats, and how to
-          use them effectively in the game.
-        </p>
-        <h2>So, why did you actually choose this subject?</h2>
-        <p>Because I then get to dive deep into the game's mechanics and able to disguise it as "For work".</p>
-      </main>
-    );
-  }
-  
+export default function AboutPage() {
+  return (
+    <>
+      <h2>Why did I pick Enlisted vehicles as a subject?</h2>
+      <p>
+        I am a huge fan of the game Enlisted, and I wanted to create a guide
+        for the armored vehicles in the game. I wanted to share my knowledge
+        and help others understand the vehicles better.
+      </p>
+
+      <h2>What is this website about?</h2>
+      <p>
+        This website is a guide for the armored vehicles in the game Enlisted.
+        It includes information about the vehicles, their stats, and how to
+        use them effectively in the game.
+      </p>
+
+      <h2>So, why did you actually choose this subject?</h2>
+      <p>
+        Because I then get to dive deep into the game's mechanics and can
+        disguise it as "For work".
+      </p>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-// src/app/page.tsx
 export default function HomePage() {
   return (
-    <main>
-      <h1>Vincent's guide on enlisted Armored Vehicles</h1>
+    <>
+      <h2>Welcome to Vincent's guide on Enlisted Armored Vehicles</h2>
       <p>Get ready to roll out!</p>
-    </main>
+
+    </>
   );
 }
